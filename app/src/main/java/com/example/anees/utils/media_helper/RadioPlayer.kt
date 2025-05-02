d0be43f5bc11d2f5b2bc0217b8e2d3f8c2f6e01e
ABDELAZIZ MAHER <abdelazizmaher17499@gmail.com>
2025-05-02 09:03:44 +0300

    Set `playWhenReady` to false after preparing the `MediaItem` in `RadioPlayer`.

--- app/src/main/java/com/example/anees/utils/media_helper/RadioPlayer.kt
+++ app/src/main/java/com/example/anees/utils/media_helper/RadioPlayer.kt
@@ -23,6 +23,7 @@ object RadioPlayer {
             val mediaItem = MediaItem.fromUri(url)
             setMediaItem(mediaItem)
             prepare()
+            playWhenReady = false
         }
     }
 
