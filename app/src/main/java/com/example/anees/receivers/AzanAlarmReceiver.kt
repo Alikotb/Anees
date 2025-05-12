8b9df9ee62a7556eb58c90cf7bb285ca2340437c
abdok <abdelrahmankamel8886@gmail.com>
2025-05-12 16:14:10 +0300

    implement the on and off azan notification feature

--- app/src/main/java/com/example/anees/receivers/AzanAlarmReceiver.kt
+++ app/src/main/java/com/example/anees/receivers/AzanAlarmReceiver.kt
@@ -3,19 +3,26 @@ package com.example.anees.receivers
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import com.example.anees.data.local.sharedpreference.SharedPreferencesImpl
 import com.example.anees.enums.PrayEnum
 import com.example.anees.ui.screens.azan.AzanOverlayActivity
+import com.example.anees.utils.Constants
 
 class AzanAlarmReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent?) {
 
         val prayEnum = intent?.getSerializableExtra("prayEnum") as? PrayEnum
-        val time = intent?.getLongExtra("time" , 0)
+        val time = intent?.getLongExtra("time", 0)
         val overlayIntent = Intent(context, AzanOverlayActivity::class.java).apply {
             flags = Intent.FLAG_ACTIVITY_NEW_TASK
-            putExtra("prayEnum" , prayEnum)
-            putExtra("time" , time)
+            putExtra("prayEnum", prayEnum)
+            putExtra("time", time)
         }
-        context.startActivity(overlayIntent)
+
+        val state = SharedPreferencesImpl(context).fetchData(Constants.AZAN_NOTIFICATION_STATE, true)
+
+        if (state) {
+            context.startActivity(overlayIntent)
+        }
     }
 }
