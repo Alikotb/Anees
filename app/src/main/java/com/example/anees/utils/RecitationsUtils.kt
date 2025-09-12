68f51f4c566daa56fa769ae5e208e26b9cf6b3e4
Abdelrahman Kamel <126878089+Abdelrahman-Kamel8886@users.noreply.github.com>
2025-09-12 17:18:03 +0300

    feat: Add more recitations and riwayat
    - Added lists for various Quranic recitations (Riwayat), including:
        - Warsh 'an Nafi'
        - Khalaf 'an Hamza
        - Al-Bazzi 'an Ibn Kathir
        - Qalun 'an Nafi'
        - Qunbul 'an Ibn Kathir
        - Al-Susi 'an Abu 'Amr
        - Qalun 'an Abu Nashit
        - Ya'qub (Rowais and Ruh)
        - Warsh 'an Abu Bakr
        - Al-Bazzi and Qunbul
        - Al-Duri 'an Al-Kisa'i
        - Al-Duri 'an Abu 'Amr
        - Shu'bah 'an 'Asim
        - Ibn Dhakwan
        - Warsh (Al-Azraq path)
        - Mushaf Al-Mu'allim
        - Mushaf Al-Mujawwad
    - Added `ABN_ZAKOON` to the `Recitations` enum.

--- app/src/main/java/com/example/anees/utils/RecitationsUtils.kt
+++ app/src/main/java/com/example/anees/utils/RecitationsUtils.kt
@@ -3,7 +3,99 @@ package com.example.anees.utils
 import com.example.anees.data.model.RecitationModel
 import com.example.anees.enums.Reciters
 
-val warshList : List<RecitationModel> = emptyList()
-val  hafsAnAsimList:List<RecitationModel> = listOf(
+val warshAnNafiList = listOf(
+    RecitationModel(Reciters.Mahmoud_Khalil_Alhusary , "https://server13.mp3quran.net/husr/Rewayat-Warsh-A-n-Nafi/"),
+    RecitationModel(Reciters.Mohamed_Saeed , "https://server16.mp3quran.net/m_sayed/Rewayat-Warsh-A-n-Nafi/"),
+    RecitationModel(Reciters.Yaseen , "https://server11.mp3quran.net/qari/"),
+    RecitationModel(Reciters.ElAyyoun_AlKouchy , "https://server11.mp3quran.net/koshi/"),
+    RecitationModel(Reciters.Ibrahim_AdDosary , "https://server10.mp3quran.net/ibrahim_dosri/Rewayat-Warsh-A-n-Nafi/"),
+    RecitationModel(Reciters.Rachid_Belalia , "https://server6.mp3quran.net/bl3/Rewayat-Warsh-A-n-Nafi/"),
+    RecitationModel(Reciters.Abdulbasit_AbdulSamad , "https://server7.mp3quran.net/basit/Rewayat-Warsh-A-n-Nafi/"),
+    RecitationModel(Reciters.Omar_AlQazabri , "https://server9.mp3quran.net/omar_warsh/"),
+)
 
+val KhalafAnHamzaList = listOf(
+    RecitationModel(Reciters.Abdurashid_Soufi,"https://server16.mp3quran.net/soufi/Rewayat-Khalaf-A-n-Hamzah/")
+)
+
+val alBazziAnIbnKathirList = listOf(
+    RecitationModel(Reciters.Ahmed_Deeban,"https://server16.mp3quran.net/deban/Rewayat-Albizi-A-n-Ibn-Katheer/"),
+    RecitationModel(Reciters.Okasha_Kamini,"https://server16.mp3quran.net/okasha/Rewayat-Albizi-A-n-Ibn-Katheer/"),
+)
+
+val QalunAnNafiList = listOf(
+    RecitationModel(Reciters.Mahmoud_Khalil_Alhusary , "https://server13.mp3quran.net/husr/Rewayat-Qalon-A-n-Nafi/"),
+    RecitationModel(Reciters.Ahmed_Altirabolsy , "https://server10.mp3quran.net/trablsi/"),
+    RecitationModel(Reciters.Eldokaly_Mohamed , "https://server7.mp3quran.net/dokali/"),
+    RecitationModel(Reciters.Tareq_Abdelghany , "https://server10.mp3quran.net/tareq/"),
+    RecitationModel(Reciters.Marwan_Alokry , "https://server16.mp3quran.net/m_akri/Rewayat-Qalon-A-n-Nafi/"),
+    RecitationModel(Reciters.Ahmed_Deeban , "https://server16.mp3quran.net/deban/Rewayat-Qalon-A-n-Nafi/"),
+    RecitationModel(Reciters.Mohamed_Abo_Senina , "https://server16.mp3quran.net/sneineh/Rewayat-Qalon-A-n-Nafi/"),
+    RecitationModel(Reciters.Mohamed_AlAmeen_Qniwa , "https://server16.mp3quran.net/qeniwa/Rewayat-Qalon-A-n-Nafi/"),
+    RecitationModel(Reciters.Ibrahim_Koshedan , "https://server16.mp3quran.net/i_kshidan/Rewayat-Qalon-A-n-Nafi/"),
+    RecitationModel(Reciters.Ali_AlHudhaifi , "https://server9.mp3quran.net/huthifi_qalon/"),
+)
+
+val QunbulAnIbnKathirList = listOf(
+    RecitationModel(Reciters.Ahmed_Deeban,"https://server16.mp3quran.net/deban/Rewayat-Qunbol-A-n-Ibn-Katheer/"),
+)
+
+val alSusiAnAbuAmrList = listOf(
+    RecitationModel(Reciters.Abdurashid_Soufi , "https://server16.mp3quran.net/soufi/Rewayat-Assosi-A-n-Abi-Amr/"),
+)
+
+val QalunAbuNashitList = listOf(
+    RecitationModel(Reciters.Waleed_Alnaeehi , "https://server9.mp3quran.net/waleed/"),
+)
+
+val YaqoubRowaisRuhList = listOf(
+    RecitationModel(Reciters.Yasser_AlMazroui , "https://server9.mp3quran.net/mzroyee/"),
+)
+
+val WarshAbuBakrList = listOf(
+    RecitationModel(Reciters.Mohamed_Abdelkarim , "https://server12.mp3quran.net/m_krm/Rewayat-Warsh-A-n-Nafi-Men-Tariq-Abi-Baker-Alasbahani/"),
+)
+
+val alBazziQunbulList = listOf(
+    RecitationModel(Reciters.Mohamed_Abdelhakim_Saeed,"https://server9.mp3quran.net/abdullah/"),
+)
+
+val alDuriAnAlKisaiList = listOf(
+    RecitationModel(Reciters.Mohamed_Abdelhakim_Saeed , "https://server9.mp3quran.net/abdullah/Rewayat-AlDorai-A-n-Al-Kisa-ai/"),
+    RecitationModel(Reciters.Moftah_Alsultany , "https://server14.mp3quran.net/muftah_sultany/Rewayat-AlDorai-A-n-Al-Kisa-ai/"),
+    RecitationModel(Reciters.Mahmoud_Elsheemy , "https://server10.mp3quran.net/sheimy/"),
+)
+
+val alDuriAnAbuAmrList = listOf(
+    RecitationModel(Reciters.Mahmoud_Khalil_Alhusary , "https://server13.mp3quran.net/husr/Rewayat-Aldori-A-n-Abi-Amr/"),
+    RecitationModel(Reciters.Moftah_Alsultany , "https://server14.mp3quran.net/muftah_sultany/Rewayat-Aldori-A-n-Abi-Amr/"),
+    RecitationModel(Reciters.Noreen_Mohamed_Sedeq , "https://server16.mp3quran.net/nourin_siddig/Rewayat-Aldori-A-n-Abi-Amr/"),
+    RecitationModel(Reciters.Elfateh_Mohamed , "https://server6.mp3quran.net/fateh/"),
+    RecitationModel(Reciters.Ahmed_Deeban , "https://server16.mp3quran.net/deban/Rewayat-Aldori-A-n-Abi-Amr/"),
+)
+
+val ShubaAnAsimList = listOf(
+    RecitationModel(Reciters.Ahmed_Deeban , "https://server16.mp3quran.net/deban/Rewayat-Sho-bah-A-n-Asim/"),
+    RecitationModel(Reciters.Ali_AlHudhaifi , "https://server9.mp3quran.net/hthfi/Rewayat-Sho-bah-A-n-Asim/"),
+)
+
+val IbnZakoonList = listOf(
+    RecitationModel(Reciters.Moftah_Alsultany , "https://server16.mp3quran.net/deban/Rewayat-Ibn-Thakwan-A-n-Ibn-Amer/"),
+)
+
+val WarshAzraqList = listOf(
+    RecitationModel(Reciters.Ahmed_Deeban , "https://server16.mp3quran.net/deban/Rewayat-Warsh-A-n-Nafi-Men-Tariq-Alazraq/")
+)
+
+val  mushafMualimList = listOf(
+    RecitationModel(Reciters.Mohamed_Seddik_Almenshawy , "https://server10.mp3quran.net/minsh/Almusshaf-Al-Mo-lim/"),
+)
+
+val mushafMujawadList = listOf(
+    RecitationModel(Reciters.Maher_Almuaiqly , "https://server12.mp3quran.net/maher/Almusshaf-Al-Mojawwad/"),
+    RecitationModel(Reciters.Mohamed_Seddik_Almenshawy , "https://server10.mp3quran.net/minsh/Almusshaf-Al-Mojawwad/"),
+    RecitationModel(Reciters.Mahmoud_Khalil_Alhusary , "https://server13.mp3quran.net/husr/Almusshaf-Al-Mojawwad/"),
+    RecitationModel(Reciters.Mahmoud_Ali_Albanna , "https://server8.mp3quran.net/bna/Almusshaf-Al-Mojawwad/"),
+    RecitationModel(Reciters.Mostafa_Ismail , "https://server8.mp3quran.net/mustafa/Almusshaf-Al-Mojawwad/"),
+    RecitationModel(Reciters.Abdulbasit_AbdulSamad , "https://server7.mp3quran.net/basit/Almusshaf-Al-Mojawwad/"),
 )
