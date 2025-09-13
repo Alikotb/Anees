377be26787187bdc55300aa3aa24718edab8a8d7
Youssef Abdelkader Allam <161586891+YoussefAllam02@users.noreply.github.com>
2025-09-13 19:42:23 +0300

    Add recitations filter in reciters screen
    This commit introduces a dropdown menu in the RecitersScreen to filter reciters based on different Quranic recitations.
    
    - Added `RecitationsDropdownMenu` composable to display and handle recitation selection.
    - Updated `RecitersScreen` to include the dropdown and manage the selected recitation state.
    - Populated `Recitations` enum with corresponding reciter lists from `RecitationsUtils.kt`.
    - Minor formatting adjustments in `RecitationsUtils.kt`.

--- app/src/main/java/com/example/anees/enums/Recitations.kt
+++ app/src/main/java/com/example/anees/enums/Recitations.kt
@@ -5,24 +5,24 @@ import com.example.anees.utils.*
 
 enum class Recitations(
     val recitationName: String,
-    val list: List<RecitationModel> = emptyList()
+    val list: List<RecitationModel>
 ) {
-    HAFS_AN_ASIM("حفص عن عاصم"),
-    WARSH_AN_NAFI("ورش عن نافع"),
-    KHALAF_AN_HAMZA("خلف عن حمزة"),
-    AL_BAZZI_AN_IBN_KATHIR("البزي عن ابن كثير"),
-    QALUN_AN_NAFI("قالون عن نافع"),
-    QUNBUL_AN_IBN_KATHIR("قنبل عن ابن كثير"),
-    AL_SUSI_AN_ABU_AMR("السوسي عن أبي عمرو"),
-    QALUN_ABU_NASHIT("قالون عن نافع من طريق أبي نشيط"),
-    YAQOUB_ROWAIS_RUH("قراءة يعقوب الحضرمي بروايتي رويس وروح"),
-    WARSH_ABU_BAKR("ورش عن نافع من طريق أبي بكر الأصبهاني"),
-    AL_BAZZI_QUNBUL("البزي وقنبل عن ابن كثير"),
-    AL_DURI_AN_AL_KISAI("الدوري عن الكسائي"),
-    AL_DURI_AN_ABU_AMR("الدوري عن أبي عمرو"),
-    SHUBA_AN_ASIM("شعبة عن عاصم"),
-    ABN_ZAKOON("ابن ذكوان عن ابن عامر"),
-    WARSH_AZRAQ("ورش عن نافع من طريق الأزرق"),
-    MUSHAF_MUALIM("المصحف المعلم"),//المصحف المرتل
-    MUSHAF_MUJAWAD("المصحف المجود");
+    HAFS_AN_ASIM("حفص عن عاصم", hafsAnAsimList),
+    WARSH_AN_NAFI("ورش عن نافع", warshAnNafiList),
+    KHALAF_AN_HAMZA("خلف عن حمزة", KhalafAnHamzaList),
+    AL_BAZZI_AN_IBN_KATHIR("البزي عن ابن كثير",alBazziAnIbnKathirList),
+    QALUN_AN_NAFI("قالون عن نافع",QalunAnNafiList),
+    QUNBUL_AN_IBN_KATHIR("قنبل عن ابن كثير",QunbulAnIbnKathirList),
+    AL_SUSI_AN_ABU_AMR("السوسي عن أبي عمرو",alSusiAnAbuAmrList),
+    QALUN_ABU_NASHIT("قالون عن نافع من طريق أبي نشيط",QalunAbuNashitList),
+    YAQOUB_ROWAIS_RUH("قراءة يعقوب الحضرمي بروايتي رويس وروح",YaqoubRowaisRuhList),
+    WARSH_ABU_BAKR("ورش عن نافع من طريق أبي بكر الأصبهاني",WarshAbuBakrList),
+    AL_BAZZI_QUNBUL("البزي وقنبل عن ابن كثير",alBazziQunbulList),
+    AL_DURI_AN_AL_KISAI("الدوري عن الكسائي",alDuriAnAlKisaiList),
+    AL_DURI_AN_ABU_AMR("الدوري عن أبي عمرو",alDuriAnAbuAmrList),
+    SHUBA_AN_ASIM("شعبة عن عاصم",ShubaAnAsimList),
+    ABN_ZAKOON("ابن ذكوان عن ابن عامر",IbnZakoonList),
+    WARSH_AZRAQ("ورش عن نافع من طريق الأزرق",WarshAzraqList),
+    MUSHAF_MUALIM("المصحف المعلم",mushafMualimList),//المصحف المرتل
+    MUSHAF_MUJAWAD("المصحف المجود",mushafMujawadList),//المصحف المجود;
 }
--- app/src/main/java/com/example/anees/ui/screens/reciters/RecitersScreen.kt
+++ app/src/main/java/com/example/anees/ui/screens/reciters/RecitersScreen.kt
@@ -17,11 +17,17 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
@@ -35,13 +41,22 @@ import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.anees.R
+import com.example.anees.enums.Recitations
 import com.example.anees.enums.RecitersEnum
 import com.example.anees.ui.screens.hadith.components.ScreenTitle
 import com.example.anees.ui.screens.radio.components.ScreenBackground
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
+import com.example.anees.ui.theme.Pink40
+import com.example.anees.ui.theme.PurpleGrey40
 
 @Preview(showBackground = true)
 @Composable
-fun RecitersScreen(onBackClick: () -> Unit = {}, navToSuraMp3: (RecitersEnum) -> Unit = {}) {
+fun RecitersScreen(
+    onBackClick: () -> Unit = {}, navToSuraMp3: (RecitersEnum) -> Unit = {}) {
+    var selectedRecitation by remember { mutableStateOf(Recitations.HAFS_AN_ASIM) }
+
     Box {
         ScreenBackground()
         Column(
@@ -51,8 +66,22 @@ fun RecitersScreen(onBackClick: () -> Unit = {}, navToSuraMp3: (RecitersEnum) ->
                 .padding(vertical = 24.dp)
         ) {
             CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Ltr) {
-                ScreenTitle( title = "قراء القرأن الكريم", onBackClick = {onBackClick() }, size = 24)
+                ScreenTitle(
+                    title = "قراء القرأن الكريم",
+                    onBackClick = { onBackClick() },
+                    size = 24
+                )
             }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            RecitationsDropdownMenu { recitation ->
+                selectedRecitation = recitation
+                println("Selected Recitation: ${recitation.recitationName}")
+            }
+
+            Spacer(modifier = Modifier.height(24.dp))
+
             LazyVerticalGrid(
                 columns = GridCells.Fixed(2),
                 verticalArrangement = Arrangement.spacedBy(12.dp),
@@ -61,14 +90,13 @@ fun RecitersScreen(onBackClick: () -> Unit = {}, navToSuraMp3: (RecitersEnum) ->
                 contentPadding = PaddingValues(bottom = 48.dp)
             ) {
                 items(RecitersEnum.entries.toTypedArray()) { reciter ->
-                    ReciterCard(reciter = reciter){
+                    ReciterCard(reciter = reciter) {
                         navToSuraMp3(it)
                     }
                 }
             }
         }
     }
-
 }
 
 @Preview(showBackground = true)
@@ -87,7 +115,7 @@ fun ReciterCard(
         shape = RoundedCornerShape(16.dp),
         elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
     ) {
-        Box{
+        Box {
             Image(
                 painter = painterResource(id = R.drawable.authorbg),
                 contentDescription = null,
@@ -122,3 +150,44 @@ fun ReciterCard(
         }
     }
 }
+
+
+@Composable
+fun RecitationsDropdownMenu(
+    onRecitationSelected: (Recitations) -> Unit
+) {
+    var expanded by remember { mutableStateOf(false) }
+    var selectedRecitation by remember { mutableStateOf(Recitations.HAFS_AN_ASIM) }
+
+    Box {
+        Button(
+            onClick = { expanded = true },
+            colors = ButtonDefaults.buttonColors(
+                containerColor =   Color(0xFF5A2E0E),
+
+                contentColor = Color.White
+            )
+        ) {
+            Text(text = selectedRecitation.recitationName)
+
+
+    }
+
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            Recitations.values().forEach { recitation ->
+                DropdownMenuItem(
+                    text = { Text(recitation.recitationName) },
+                    onClick = {
+                        selectedRecitation = recitation
+                        expanded = false
+                        onRecitationSelected(recitation)
+                    }
+                )
+            }
+        }
+    }
+}
+
